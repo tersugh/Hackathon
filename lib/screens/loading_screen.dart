75e0839e992aa92d35926116a7bffdf4ce268d42
lordvidex <lordvidex.prime@gmail.com>
2021-09-18 14:08:23 +0300

    Template loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({ Key? key }) : super(key: key);
+  const LoadingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        body: Column(children: const [
+      Expanded(child: Placeholder()),
+      Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+          ),
+          child: Text('Loading...'))
+    ]));
   }
 }
